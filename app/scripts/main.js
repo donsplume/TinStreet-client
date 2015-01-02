d75da878feaa43dc0511caade697f214e1e0d703
Phil Richman <p.d.richman@gmail.com>
2015-01-02 23:58:25 +0100

    Add resource to get collection and add to it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,41 @@
 'use strict';
 
-angular.module('tinstreet', [])
+angular.module('tinstreet', ['ngResource','autocomplete'])
 
-.controller('albumController', ['$http', function ($http) {
-    var album = this;
-    album.rows = [];
-    $http.get('api/album/all_the_lands').success(function(data){
-        album.rows = data;
+.factory('tsFinder', function () {
+    return {
+        getCards: function (query) {
+
+        }
+    }
+})
+
+.factory('tsCollection', function ($resource) {
+    return $resource('api/user/:username/collection/:code', {username:'@_username'}, {
+        'create': {method:'POST'},
+        'read':   {method:'GET',isArray:true},
+        'update': {method:'PUT'}
     });
+})
+
+.controller('CollectionController', ['$http', '$scope', 'tsCollection', function ($http,$scope,tsCollection) {
+    $scope.cards = ['deathrite shaman']
+
+    var collection = this;
+
+    collection.items = tsCollection.read({username:'phil'})
+
+    $scope.getCards = function (text) {
+        $http({'url':'api/card','method':'GET','params':{'name':text}}).success(function(data){
+            $scope.cards=data;
+        });
+    };
+
+    $scope.add = function ( printing ) {
+        printing._username = 'phil';
+        tsCollection.create(printing, function(data) {
+            console.log(data);
+            collection.items.push(data);
+        });
+    };
 }]);
